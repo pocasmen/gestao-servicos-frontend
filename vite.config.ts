df952a7929585d986511a7979b65a381e372f252
pocasmen <pb.malheiro@gmail.com>
2026-05-20 16:19:13 +0100

    versão 1.2.4 vite.config.ts 3

--- vite.config.ts
+++ vite.config.ts
@@ -34,39 +34,19 @@ export default defineConfig(({ command }) => {
       'Cache-Control': 'no-store, no-cache, must-revalidate',
     },
   },
+  preview: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+      }
+    }
+  },
   build: {
     minify: 'esbuild',
     sourcemap: true,
-    rollupOptions: {
-      treeshake: true,
-      input: path.resolve(__dirname, 'index.html'),
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('scheduler') || id.includes('react-dom') || id.includes('react-router')) {
-              return 'vendor-react';
-            }
-            if (id.includes('bootstrap') || id.includes('react-bootstrap') || id.includes('lucide')) {
-              return 'vendor-ui';
-            }
-            if (id.includes('supabase')) {
-              return 'vendor-supabase';
-            }
-            if (id.includes('react-big-calendar') || id.includes('date-fns') || id.includes('react-datepicker')) {
-              return 'vendor-calendar';
-            }
-            if (id.includes('html2pdf.js')) {
-              return 'vendor-pdf';
-            }
-            if (id.includes('axios')) {
-              return 'vendor-utils';
-            }
-            return 'vendor';
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 1500,
   },
   envPrefix: 'VITE',
   test: {
